86127e29d7995007c2e6871df017a5c14ec1984e
daviaraujo-techsolutio <daviaraujo-techsolutio@users.noreply.github.com>
2026-01-28 15:41:37 -0300

    rollback in LayerArchitectureTest

--- src/test/java/com/davijaf/hexagonal/architecture/LayerArchitectureTest.java
+++ src/test/java/com/davijaf/hexagonal/architecture/LayerArchitectureTest.java
@@ -1,56 +1,43 @@
 package com.davijaf.hexagonal.architecture;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.library.Architectures;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-class LayerArchitectureTest {
+@AnalyzeClasses(packages = "com.davijaf.hexagonal", importOptions = ImportOption.DoNotIncludeTests.class)
+public class LayerArchitectureTest {
 
-    private static JavaClasses importedClasses;
+    @ArchTest
+    public static final Architectures.LayeredArchitecture LAYER_ARCHITECTURE_TEST =
+            layeredArchitecture()
+                    .consideringAllDependencies()
+                    // Definição das camadas
+                    .layer("AdapterIn").definedBy("..adapters.in..")
+                    .layer("AdapterOut").definedBy("..adapters.out..")
+                    .layer("UseCase").definedBy("..application.core.usecase..")
+                    .layer("PortIn").definedBy("..application.ports.in..")
+                    .layer("PortOut").definedBy("..application.ports.out..")
+                    .layer("Config").definedBy("..config..")
 
-    @BeforeAll
-    static void setup() {
-        importedClasses = new ClassFileImporter()
-                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                .importPackages("com.davijaf.hexagonal");
-    }
+                    // Restrições de acesso entre camadas
+                    // AdapterIn só pode ser acessado por Config
+                    .whereLayer("AdapterIn").mayOnlyBeAccessedByLayers("Config")
 
-    @Test
-    void shouldRespectHexagonalArchitectureLayers() {
-        // Definição das camadas e restrições de acesso
-        layeredArchitecture()
-                .consideringAllDependencies()
-                // Definição das camadas
-                .layer("AdapterIn").definedBy("..adapters.in..")
-                .layer("AdapterOut").definedBy("..adapters.out..")
-                .layer("UseCase").definedBy("..application.core.usecase..")
-                .layer("PortIn").definedBy("..application.ports.in..")
-                .layer("PortOut").definedBy("..application.ports.out..")
-                .layer("Config").definedBy("..config..")
+                    // AdapterOut só pode ser acessado por Config
+                    .whereLayer("AdapterOut").mayOnlyBeAccessedByLayers("Config")
 
-                // Restrições de acesso entre camadas
-                // AdapterIn só pode ser acessado por Config
-                .whereLayer("AdapterIn").mayOnlyBeAccessedByLayers("Config")
+                    // UseCase só pode ser acessado por Config
+                    .whereLayer("UseCase").mayOnlyBeAccessedByLayers("Config")
 
-                // AdapterOut só pode ser acessado por Config
-                .whereLayer("AdapterOut").mayOnlyBeAccessedByLayers("Config")
+                    // PortIn pode ser acessado por UseCase, AdapterIn e Config
+                    .whereLayer("PortIn").mayOnlyBeAccessedByLayers("UseCase", "AdapterIn", "Config")
 
-                // UseCase só pode ser acessado por Config
-                .whereLayer("UseCase").mayOnlyBeAccessedByLayers("Config")
+                    // PortOut pode ser acessado por UseCase, AdapterOut e Config
+                    .whereLayer("PortOut").mayOnlyBeAccessedByLayers("UseCase", "AdapterOut", "Config")
 
-                // PortIn pode ser acessado por UseCase, AdapterIn e Config
-                .whereLayer("PortIn").mayOnlyBeAccessedByLayers("UseCase", "AdapterIn", "Config")
-
-                // PortOut pode ser acessado por UseCase, AdapterOut e Config
-                .whereLayer("PortOut").mayOnlyBeAccessedByLayers("UseCase", "AdapterOut", "Config")
-
-                // Config não pode ser acessado por nenhuma camada
-                .whereLayer("Config").mayNotBeAccessedByAnyLayer()
-
-                .check(importedClasses);
-    }
+                    // Config não pode ser acessado por nenhuma camada
+                    .whereLayer("Config").mayNotBeAccessedByAnyLayer();
 }
